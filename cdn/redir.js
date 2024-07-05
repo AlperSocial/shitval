34ef882efb0731776c45c2a60b59b85fa025a7cb
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2024-07-05 14:15:36 +0200

    Update redir.js

--- cdn/redir.js
+++ cdn/redir.js
@@ -1,27 +1,26 @@
 function redirect(urltext) {
-  const url = new URL(urltext);
-  if (url.hostname == "discord.com") { return window.open(urltext, "_blank"); }
-  
-  swal({
-    title: "Hold on!",
-    text: "You are leaving shitval.top, are you sure you want to go to '" + url.hostname + "'?",
-    icon: "warning",
-    buttons: {
-        cancel: "Stay on shitval.top",
-        yes: "Redirect me to " + url.hostname,
-    },
-  }).then((value) => {
-    switch (value) {
-      case "yes":
-        window.open(urltext, "_blank")
-        break;
-      default:
-        break;
-    }
-  });
+    const url = new URL(urltext);
+    if (url.hostname == "discord.com") { return window.open(urltext, "_blank"); }
+    Swal.fire({
+      title: "Hold on!",
+      text: "You are leaving shitval.top, are you sure you want to go to '" + url.hostname + "'?",
+      icon: "warning",
+      buttons: {
+          cancel: "Stay on shitval.top",
+          yes: "Redirect me to " + url.hostname,
+      },
+    }).then((value) => {
+      switch (value) {
+        case "yes":
+          window.open(urltext, "_blank")
+          break;
+        default:
+          break;
+      }
+    });
 }
 $(document).on('click', 'a', function(e) {
-  redirect(e.target.href)
+    redirect(e.target.parentElement.href)
     e.preventDefault();
     return false;
 });
